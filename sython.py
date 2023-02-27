ec7551ef1688a9c62f2f7999a4c65b59b508a659
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-02-27 05:27:33 +0300

    Update sython.py

--- sython.py
+++ sython.py
@@ -562,7 +562,50 @@ async def update(event):
     await event.edit("**جاري اعادة تشغيل السورس**")
     await sython.disconnect()
     await sython.send_message("me", "**اكتملت اعادة تشغيل السورس**")
+c = requests.session()
+bot_username = '@t06bot'
 
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع المليار"))
+async def _(event):
+        await event.edit("حسنا, تأكد من انك مشترك ب قنوات الاشتراك الاجباري لتجنب الأخطأء")
+        channel_entity = await sython.get_entity(bot_username)
+        await sython.send_message('@t06bot', 'جاري التجميع بواسطة | SOMY TEAM')
+        channel_entity = await sython.get_entity(bot_username)
+        await sython.send_message('@t06bot', '/start')
+        await asyncio.sleep(5)
+        msg0 = await sython.get_messages('@t06bot', limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(5)
+        msg1 = await sython.get_messages('@t06bot', limit=1)
+        await msg1[0].click(0)
+
+        chs = 1
+        for i in range(100):
+
+            await asyncio.sleep(5)
+
+            list = await sython(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await sython.send_message(event.chat_id, f"لايوجد قنوات  في البوت | SY")
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await sython(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await sython(ImportChatInviteRequest(bott))
+                msg2 = await sython.get_messages('@t06bot', limit=1)
+                await msg2[0].click(text='التالي')
+                chs += 1
+                
+            except:
+                await sython.send_message(event.chat_id, f"خطأ من المحتمل تم حظر الانضمام ")
+                break
+        await sython.send_message(event.chat_id, "تم الانتهاء من التجميع !")
 
 print("♦️ - sython Userbot Running - ♦️")
 sython.run_until_disconnected()
