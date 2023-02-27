96d817fd242e24de574a8ce4f46694038bb90167
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-02-27 05:04:20 +0300

    Update points.py

--- points.py
+++ points.py
@@ -10,30 +10,30 @@ c = requests.session()
 bot_username = '@t06bot'
 
 
-@sython.on(events.NewMessage(outgoing=True, pattern=r".تجميع المليار"))
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع المليار"))
 async def _(event):
-    if ispay[0] == "yes":
         await event.edit("حسنا, تأكد من انك مشترك ب قنوات الاشتراك الاجباري لتجنب الأخطأء")
         channel_entity = await sython.get_entity(bot_username)
+        await sython.send_message('@t06bot', 'جاري التجميع بواسطة | SOMY TEAM')
+        channel_entity = await sython.get_entity(bot_username)
         await sython.send_message('@t06bot', '/start')
-        await asyncio.sleep(0)
+        await asyncio.sleep(5)
         msg0 = await sython.get_messages('@t06bot', limit=1)
-        await msg0[0].click(0)
-        await asyncio.sleep(0)
+        await msg0[0].click(2)
+        await asyncio.sleep(5)
         msg1 = await sython.get_messages('@t06bot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
         for i in range(100):
-            if ispay[0] == 'no':
-                break
-            await asyncio.sleep(0)
+
+            await asyncio.sleep(5)
 
             list = await sython(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             msgs = list.messages[0]
             if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
-                await sython.send_message(event.chat_id, f"مافي قنوات بلبوت")
+                await sython.send_message(event.chat_id, f"لايوجد قنوات  في البوت | SY")
                 break
             url = msgs.reply_markup.rows[0].buttons[0].url
             try:
@@ -43,13 +43,11 @@ async def _(event):
                     bott = url.split('/')[-1]
                     await sython(ImportChatInviteRequest(bott))
                 msg2 = await sython.get_messages('@t06bot', limit=1)
-                await msg2[0].click(text='التالي')
+                await msg2[0].click(text='تحقق')
                 chs += 1
-                await sython.send_message(event.chat_id, f"تم الاشتراك في {chs} قناة")
+                
             except:
-                await sython.send_message(event.chat_id, f"خطأ , ممكن تبندت")
+                await sython.send_message(event.chat_id, f"خطأ من المحتمل تم حظر الانضمام ")
                 break
         await sython.send_message(event.chat_id, "تم الانتهاء من التجميع !")
 
-    else:
-        await event.edit("يجب الدفع لاستعمال هذا الامر !")
